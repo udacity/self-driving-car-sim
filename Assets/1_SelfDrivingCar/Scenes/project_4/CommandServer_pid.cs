eb9501070b217acd988586073ab13bf6e1b5f7cd
Graciela Gonzalez <gmtlali88@gmail.com>
2018-05-17 14:28:53 -0700

    PID changes to command server and new socket client

---
+++ Assets/1_SelfDrivingCar/Scenes/project_4/CommandServer_pid.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+using System;
+using UnityEngine.SceneManagement;
+using System.Security.AccessControl;
+
+
+
+
+public class CommandServer_pid : MonoBehaviour
+{
+	public CarRemoteControl CarRemoteControl;
+	public Camera FrontFacingCamera;
+	// private SocketIOComponent _socket;
+	private SocketClient_pid client;
+	private CarController _carController;
+	private WaypointTracker_pid wpt;
+
+	// Use this for initialization
+	void Start()
+	{
+		// client = GameObject.Find("SocketIO").GetComponent<SocketIOComponent>();
+		client = GameObject.Find("SocketClient").GetComponent<SocketClient_pid>();
+		client.On("open", OnOpen);
+		client.On ("reset", OnReset);
+		client.On("steer", OnSteer);
+		client.On("manual", onManual);
+		_carController = CarRemoteControl.GetComponent<CarController>();
+		wpt = new WaypointTracker_pid ();
+		// init_status = true;
+
+		Application.ExternalCall("mySetupFunction");
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	void OnOpen(JSONObject jsonObject)
+	{
+		Debug.Log("Connection Open");
+		EmitTelemetry();
+	}
+
+	// 
+	void onManual(JSONObject jsonObject)
+	{
+        Debug.Log("Manual driving event ...");
+		EmitTelemetry ();
+	}
+
+	void OnReset(JSONObject jsonObject)
+	{
+		SceneManager.LoadScene("LakeTrackAutonomous_pid");
+		EmitTelemetry ();
+	}
+
+	void OnSteer(JSONObject jsonObject)
+	{
+        Debug.Log("Steering data event ...");
+		//JSONObject jsonObject = obj.data;
+		JSONObject obj = jsonObject;
+
+		CarRemoteControl.SteeringAngle = float.Parse(jsonObject.GetField("steering_angle").ToString());
+		CarRemoteControl.Acceleration = float.Parse(jsonObject.GetField("throttle").ToString());
+		var steering_bias = 1.0f * Mathf.Deg2Rad;
+		CarRemoteControl.SteeringAngle += steering_bias;
+		EmitTelemetry();
+	}
+
+	void EmitTelemetry()
+	{
+		// UnityMainThreadDispatcher.Instance().Enqueue(() =>
+		// {
+			print("Attempting to Send...");
+			// send only if it's not being manually driven
+			if ((Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.S))) {
+				client.Emit("telemetry", new JSONObject());
+			} else {
+				// Collect Data from the Car
+				Dictionary<string, string> data = new Dictionary<string, string>();
+				var cte = wpt.CrossTrackError (_carController);
+				data["steering_angle"] = _carController.CurrentSteerAngle.ToString("N4");
+				data["throttle"] = _carController.AccelInput.ToString("N4");
+				data["speed"] = _carController.CurrentSpeed.ToString("N4");
+				data["cte"] = cte.ToString("N4");
+				data["image"] = Convert.ToBase64String(CameraHelper.CaptureFrame(FrontFacingCamera));
+				client.Emit("telemetry", new JSONObject(data));
+			}
+		// });
+				
+	}
+}
